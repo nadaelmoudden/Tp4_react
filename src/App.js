9c17e4e001da03d99f457d9c8e2d9af9efc96841
nadaelmoudden <148474235+nadaelmoudden@users.noreply.github.com>
2023-11-18 18:33:02 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react';
 
 function App() {
+  useEffect(() => {
+    fetch('https://api.thecatapi.com/v1/images/search')
+      .then((response) => response.json())
+      .then((data) => {
+        const imgElement = document.createElement('img');
+        imgElement.src = data[0].url;
+        imgElement.style.width = '300px';
+        imgElement.style.height = '300px';
+        imgElement.style.display = 'block'; // Make the image a block-level element
+        imgElement.style.margin = 'auto'; // Center the image horizontally
+        imgElement.style.marginTop = '20vh'; // Center the image vertically (50% from the top)
+
+        const appContainer = document.querySelector('.App');
+        appContainer.innerHTML = ''; // Clear any previous content
+        appContainer.appendChild(imgElement);
+      })
+      .catch((error) => {
+        console.error('An error occurred:', error);
+      });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="App"></div>
   );
 }
 
